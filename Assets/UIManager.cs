d800d2e8842b84b7d05041684cc01bc6c21aca1c
iulian098 <craciuniulian25@gmail.com>
2024-03-21 20:37:41 +0200

    Main Menu Scene

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
+using UnityEditor.Build.Content;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
@@ -8,18 +7,22 @@ public class UIManager : MonoBehaviour
     [SerializeField] Vehicle vehicle;
     [SerializeField] TMP_Text kmText;
     [SerializeField] TMP_Text gearText;
-    [SerializeField] TMP_Text absText;
-    [SerializeField] TMP_Text tcsText;
     [SerializeField] TMP_Text rpmText;
+    [SerializeField] TMP_Text startingTimeText;
 
-    void Update()
+    void FixedUpdate()
     {
         if (vehicle == null) return;
 
+        if (RaceManager.Instance.CurrentStartingTime > 0 && RaceManager.Instance.CurrentState == RaceManager.State.Starting) {
+            startingTimeText.gameObject.SetActive(true);
+            startingTimeText.text = Mathf.CeilToInt(RaceManager.Instance.CurrentStartingTime).ToString();
+        }
+        else
+            startingTimeText.gameObject.SetActive(false);
+
         kmText.text = "Km/h: " + vehicle.Kmph;
         gearText.text = "Gear: " + vehicle.CurrentGear;
-        absText.text = "ABS: " + vehicle.ABS;
-        tcsText.text = "TCS: " + vehicle.TCS;
         rpmText.text = "RPM: " + vehicle.EngineRPM;
     }
 }
